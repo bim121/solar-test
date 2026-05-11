80718a62ad06dd1e84dfe6617fcbd095d8419b09
Vitaliy Tishik <vitisik54@gmail.com>
2026-05-11 19:24:27 +0300

    add test

---
+++ src/test/java/ClockServiceTest.java
@@ -0,0 +1,18 @@
+import org.example.ClockService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClockServiceTest {
+
+    @Test
+    void shouldReturnCurrentTime() {
+
+        ClockService service = new ClockService();
+
+        String time = service.getCurrentTime();
+
+        assertNotNull(time);
+        assertFalse(time.isEmpty());
+    }
+}
